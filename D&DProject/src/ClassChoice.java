7894187594f99432db5f7fe80b3abef1c065ccba
emily.deline <emily.deline@MU-C1-04.mullen.pvt.k12.co.us>
2019-09-19 11:55:34 -0600

    COME ON!! You have to finish this by Friday

--- D&DProject/src/ClassChoice.java
+++ D&DProject/src/ClassChoice.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class ClassChoice
 	{
-
+		static String gameName;
+		static String class2;
 		public static void classChoice()
 		
 			{
@@ -10,26 +11,26 @@ public class ClassChoice
 				
 				System.out.println("Okay, so lets start out with makeing a name that you want to be called thought out the campaign. So what is your players name?");
 				Scanner gameNameInput = new Scanner(System.in);
-				String gameName = gameNameInput.nextLine();
+				gameName = gameNameInput.nextLine();
 				System.out.println("Thats an awesome name! " + gameName);
 				
 				// have to choose a trait
 				
 				System.out.println(gameName + ", lets start with choosing your class.");
+				System.out.println("Enter the corresponding number for the class of your choice. ");
 				
-				System.out.println("Enter the corresponding number for the class of your choice. "
-						+ "1 Fighter"
-						+ "2 Barbarian"
-						+ "3 Paladin"
-						+ "4 Bard"
-						+ "5 Sorcerer"
-						+ "6 Warlock"
-						+ "7 Rogue"
-						+ "8 Cleric"
-						+ "9 Druid"
-						+ "10 Monk"
-						+ "11 Ranger"
-						+ "12 Wizard");
+					System.out.println(" 1 Fighter");
+					System.out.println(" 2 Barbarian");
+					System.out.println(" 3 Paladin");
+					System.out.println(" 4 Bard");
+					System.out.println(" 5 Sorcerer");
+					System.out.println(" 6 Warlock");
+					System.out.println(" 7 Rogue");
+					System.out.println(" 8 Cleric");
+					System.out.println(" 9 Druid");
+					System.out.println(" 10 Monk");
+					System.out.println(" 11 Ranger");
+					System.out.println(" 12 Wizard");
 				
 				Scanner classChoice1 = new Scanner(System.in);
 				int class1 = classChoice1.nextInt();
@@ -38,41 +39,56 @@ public class ClassChoice
 					{
 						System.out.println("YAY I see an espiering Fighter!");
 						System.out.println("A Fighter is a master of martial combat, skilled with a variety of weapns and armor. OOOOO fancy!");
+						class2 = "Fighter";
 					}
 				else if (class1 == 2)
 					{
 						System.out.println("YAY I see a verocious Barbarian in our mists!");
 						System.out.println("A Barbarian is a fierce warrior of primitive background who can enter a battle rage. YAY!! Sounds like fun.");
+						class2 = "Barbarian";
+
 					}
 				else if (class1 == 3)
 					{
 						System.out.println("YAY I see a couragious Paladin over here!");
 						System.out.println("A Paladin is a holy warrior bound to a sacred oath. I wonder what is your oath, but thats a later conversation");
+						class2 = "Paladin";
+
 					}
 				else if (class1 == 4)
 					{
 						System.out.println("OOOOO Nice a Bard, now you can sing though out the campign!");
 						System.out.println("A Bard is an inspiring magician whose power echos the music of creation. Now that is music to my ears.");
+						class2 = "Bard";
+
 					}
 				else if (class1 == 5)
 					{
 						System.out.println("A fancy Sorcerer hmmm, that is nice spell caseting right there.");
 						System.out.println("A Sorcerer is a spellcaster who draws on inherent magic from a gift or a bloodline. Family ties am I right.");
+						class2 = "Sorcerer";
+
 					}
 				else if (class1 == 6)
 					{
 						System.out.println ("YAY! A wonderful Warlock, now that is some power!");
 						System.out.println("A Warlock is a weilder of magic that is derived from a bargain with an extraplanar entity. Now those are some wonderful communication skills.");
+						class2 = "Warlock";
+
 					}
 				else if (class1 == 7)
 					{
 						System.out.println("Theres a scrappy Rogue right next to me. Nice choice " + gameName);
 						System.out.println("A Rogue is a scounderal who uses stealth and trickery to overcome obstacles and enemies. Sounds like my kind of person!");
+						class2 = "Rogue";
+
 					}
 				else if (class1 == 8)
 					{
 						System.out.println("Cleric, you are *I (the DM) bows towards you*");
 						System.out.println("A Cleric is a priestly champion who wields divine magic in service of a higer power.");
+						class2 = "Cleric";
+
 					}
 				else if (class1 == 9)
 					{
@@ -83,19 +99,23 @@ public class ClassChoice
 					{
 						System.out.println("A chill Monk, GREAT CHOICE!");
 						System.out.println("A Monk is a master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection. Knowing yourself and your moloral code, dang I wish!");
+						class2 = "Monk";
+
 					}
 				else if (class1 == 11)
 					{
 						System.out.println("YAY I see a roaming Rnager, over the hills!");
 						System.out.println("A Ranger is a warrior who ises martial prowess and nature magic to combat threats on the edges of civilization. A protector of nature, umm yes please!");
+						class2 = "Ranger";
+
 					}		
 				else if (class1 == 12)
 					{
 						System.out.println( gameName + ", you are a Wizard! I bet you are going to make one of, if not, the best Wizards I have ever seen!");
 						System.out.println("A Wizard is a scholarly magic - user capable of manipulating the structures of reality. That is the coolest thing I think I have ever heard!");
-						
+						class2 = "Wizard";
+
 					}
-				System.out.println("All we know about you so far is that your name is " + gameName + " and that your class is " + classChoice1);
 				// defining the class
 				
 						}
--- D&DProject/src/RaceChoice.java
+++ D&DProject/src/RaceChoice.java
@@ -1,13 +1,79 @@
+import java.util.Scanner;
 
 public class RaceChoice
 	{
-
+		static int race1;
+		static String race;
 		public static void raceChoice()
 			{
 				System.out.println("Now its time to choose your race!");
-				System.out.println("The races you can choose from are as follows: Dwarf, Elf, Gnome, Halfing, Human, Dragonborn, Tiefling, Half-Orc, or Half-Elf."
-						+ "Just enter, you know the drill, 1 for a Dwarf, 2 for an Elf, 3 for a Gnome, 4 for a Halfing, 5 for a Dragonborn, 6 for a Tiefling, 7 for a Half-Orc, or 8 for a Helf-Elf.");
+				System.out.println("The races you can choose from are as follows. You know the drill.");
 				
+				System.out.println(" 1 Dwarf");
+				System.out.println(" 2 Elf");
+				System.out.println(" 3 Gnome");
+				System.out.println(" 4 Halfing");
+				System.out.println(" 5 Dragonborn");
+				System.out.println(" 6 Tiefling");
+				System.out.println(" 7 Half-Orc");
+				System.out.println(" 8 Half-Elf");
+				
+				Scanner fred = new Scanner(System.in);
+				int race1 = fred.nextInt();
+				
+				if (race1 == 1)
+					{
+						System.out.println("You have chosen to be a Dwarf. That means you have two options...");
+						System.out.println("You can either be:");
+						
+						System.out.println("1 Hill Dwarf");
+						System.out.println("Or 2 Mountian Dwarf");
+						
+						Scanner dwarf = new Scanner(System.in);
+						int dwarfChoice = dwarf.nextInt();
+						
+							if (dwarfChoice == 1)
+								{
+									System.out.println("Cool a Hill Dwarf");
+									race = "Hill Dwarf";
+								}
+							else if (dwarfChoice == 2)
+								{
+									System.out.println("YAY! A Mountain Dwarf, I like the mountains better anyways.");
+									race = "Mountian Dwarf";
+								}
+							System.out.println("Nice to meet you " + ClassChoice.gameName + " the " + RaceChoice.race + " " + ClassChoice.class2);
+
+					}
+				else if (race1 == 2)
+					{
+						System.out.println("You have chosen to be a Elf");
+						race = "Elf";
+					}
+				else if (race1 == 3)
+					{
+						System.out.println("You have chosen to be a Gnome");					
+					}
+				else if (race1 == 4)
+					{
+						System.out.println("You have chosen to be a Halfling");					
+					}
+				else if (race1 == 5)
+					{
+						System.out.println("You have chosen to be a Dragonborn");						
+					}
+				else if (race1 == 6)
+					{
+						System.out.println("You have chosen to be a Tiefling");						
+					}
+				else if (race1 == 7)
+					{
+						System.out.println("You have chosen to be a Half-Orc");						
+					}
+				else if (race1 == 8)
+					{
+						System.out.println("You have chosen to be a Half-Elf");						
+					}
 			}
 
 	}
